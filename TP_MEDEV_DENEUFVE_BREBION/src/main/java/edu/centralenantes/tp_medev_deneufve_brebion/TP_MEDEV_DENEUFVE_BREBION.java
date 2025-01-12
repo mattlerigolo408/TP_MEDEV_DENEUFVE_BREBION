e9c039efa61c6880311409594c4a8cb303815ad8
mattlerigolo <matt.brebion@orange.fr>
2025-01-12 13:06:13 +0100

    initialisation projet

---
+++ TP_MEDEV_DENEUFVE_BREBION/src/main/java/edu/centralenantes/tp_medev_deneufve_brebion/TP_MEDEV_DENEUFVE_BREBION.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.centralenantes.tp_medev_deneufve_brebion;
+
+/**
+ *
+ * @author mattlerigolo
+ */
+public class TP_MEDEV_DENEUFVE_BREBION {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
